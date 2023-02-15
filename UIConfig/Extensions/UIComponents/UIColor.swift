5709d8c2e64751f43a2416e381e4091b3897818b
Juan <juanjoseen@gmail.com>
2023-02-15 17:48:27 -0600

    [UPDATE] bgColor in lightmode

--- UIConfig/Extensions/UIComponents/UIColor.swift
+++ UIConfig/Extensions/UIComponents/UIColor.swift
@@ -16,7 +16,7 @@ public extension UIColor {
     
     static var textColor: UIColor {
         return UIColor { (traits) -> UIColor in
-            return traits.userInterfaceStyle == .dark ? UIColor(red: 0.969, green: 0.973, blue: 0.980, alpha: 1) : UIColor(red: 0.106, green: 0.102, blue: 0.157, alpha: 1)
+            return traits.userInterfaceStyle == .dark ? UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.00) : UIColor(red: 0.106, green: 0.102, blue: 0.157, alpha: 1)
         }
     }
     
@@ -26,7 +26,7 @@ public extension UIColor {
     
     static var bgColor: UIColor {
         return UIColor { (traits) -> UIColor in
-            return traits.userInterfaceStyle == .dark ? UIColor(red: 0.05, green: 0.07, blue: 0.12, alpha: 1.00) : UIColor(red: 0.97, green: 0.97, blue: 0.98, alpha: 1.00)
+            return traits.userInterfaceStyle == .dark ? UIColor(red: 0.05, green: 0.07, blue: 0.12, alpha: 1.00) : UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.00)
         }
     }
     
